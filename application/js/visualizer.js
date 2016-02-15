0b3c5ce27a7f3c718a2e3a0c890c4c938ec0f9f0
MikaSez <mis.strator@gmail.com>
2016-02-15 23:06:54 +0100

    Amelioration visuel son

--- application/js/visualizer.js
+++ application/js/visualizer.js
@@ -8,12 +8,24 @@
 		var config = conf || { };
 		var animation;
 		var bufferLength = config.bufferLength || 1024;
-
+        var lastSoundWaves = [];
+        var numberSoundWavesKeep = 2;
+        var skipWaves = 50;
+        var currentWaves = 1;
+        
 		var dataArray = new Uint8Array();
 		var worker = new Worker("js/visualizerWorker.js");
 
-
-
+        var Point = function(pointX, pointY){
+            
+            var x = pointX;
+            var y = pointY;
+            
+            return {
+                x,y
+            };
+        }
+        
 		worker.onmessage = function(event){
 			switch(event.data.command) {
 				case 'process': 
@@ -44,43 +56,83 @@
 		}
 		
 		
+        
+        function pushSoundWave(points){
+            currentWaves++;
+            if(currentWaves%skipWaves==0){
+                 if(lastSoundWaves.length >= numberSoundWavesKeep){
+                     lastSoundWaves.pop();
+                }
+                lastSoundWaves.push(points);     
+            }
+             
+        }
 
 		 
 	    var draw = function() {  
+            var soundWave = [];
+            
 		    canvasCtx.save();  
 		   
 		    analyser.getByteTimeDomainData(dataArray);
-		    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+		    canvasCtx.fillStyle = 'rgb(0,0,0)';
 		    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-		    canvasCtx.lineWidth = 1;
+		    canvasCtx.lineWidth = 5;
 
-		    var gradient=canvasCtx.createLinearGradient(0,0,170,0);
-			gradient.addColorStop("0","magenta");
-			gradient.addColorStop("0.5","blue");
-			gradient.addColorStop("1.0","red");
-
-		    canvasCtx.strokeStyle = gradient;
+		    canvasCtx.strokeStyle = "purple";
 		    canvasCtx.beginPath();
-		    var sliceWidth = canvas.width * 1.0 / bufferLength;
+		    var sliceWidth = canvas.width / bufferLength;
 		    var x = 0;
 		      
 		    for(var i = 0; i < bufferLength; i++) {
+              
 		      var v = dataArray[i] / 128.0;
 		      var y = v * canvas.height/2;
 
 		      if(i === 0) {
 		        canvasCtx.moveTo(x, y);
 		      } else {
-		        canvasCtx.lineTo(x, y);
+                canvasCtx.lineTo(x, y); 
+                soundWave.push(new Point(x, y));
 		      }
-
 		      x += sliceWidth;
 		    }
+            
 		    canvasCtx.lineTo(canvas.width, canvas.height/2);
-		    canvasCtx.stroke(); 
-		    canvasCtx.restore();
+            canvasCtx.lineTo(0, canvas.height/2);
+            canvasCtx.stroke();
+            canvasCtx.restore();
+            drawSoundWaves();
+            pushSoundWave();
+		    
 	  };
-
+      
+      function drawSoundWaves(){
+           
+          console.log("drawing sound + " + lastSoundWaves.length);
+          canvasCtx.strokeStyle = getRandomColor();
+          canvasCtx.lineWidth = 0.3;
+          for(wave in lastSoundWaves){
+              canvasCtx.save();  
+              for(p in wave){
+                 canvasCtx.lineTo(p.x, p.y);     
+              }
+              canvasCtx.lineTo(canvas.width, canvas.height/2);
+              canvasCtx.lineTo(0, canvas.height/2);
+              canvasCtx.stroke(); 
+              canvasCtx.restore();  
+          }
+      }
+        
+        
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 	  var update = function(){
 	  	draw();
 	  	animation = requestAnimationFrame(update);  
